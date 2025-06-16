af92873c111b6de1f3d948d837a6ba2ed811c881
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-06-16 17:02:18 +0300

    Add project files.

---
+++ chatAppServer.Webapi/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace chatAppServer.Webapi.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+       
+        }
+    }
+}
