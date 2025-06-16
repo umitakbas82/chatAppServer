85041ea44bc67a93853f2e548e8f2f744006fee4
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-06-16 17:10:56 +0300

    first commit

---
+++ chatAppServer.Webapi/Models/Chat.cs
@@ -0,0 +1,16 @@
+﻿namespace chatAppServer.Webapi.Models
+{
+    public sealed class Chat
+    {
+        public Chat()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ToUserId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+    }
+}
---
+++ chatAppServer.Webapi/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace chatAppServer.Webapi.Models
+{
+    public sealed class User
+
+    {
+     
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Avatar { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+
+
+
+
+    }
+}
