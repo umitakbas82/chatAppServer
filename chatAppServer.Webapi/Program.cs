2ed98b0fb59d78a92319a8c45ee1c3c1d8c51681
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-06-18 16:20:11 +0300

    cors policy updated

--- chatAppServer.Webapi/Program.cs
+++ chatAppServer.Webapi/Program.cs
@@ -2,7 +2,14 @@ using chatAppServer.Webapi.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("SqlServer")));
